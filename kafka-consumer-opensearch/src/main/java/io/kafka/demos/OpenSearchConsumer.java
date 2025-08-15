0ab805f9a6b83bc08dc71c745ffcb336f6e300be
Jabir <15pwele4721@uetpeshawar.edu.pk>
2025-08-15 20:20:16 +0400

    updated OpenSearchConsumer

--- kafka-consumer-opensearch/src/main/java/io/kafka/demos/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/kafka/demos/OpenSearchConsumer.java
@@ -24,6 +24,7 @@ import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 import java.io.IOException;
 import java.net.URI;
 import java.time.Duration;
